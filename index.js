6274f4d0caacf853a659db4ed5812ec296c3cace
michelle nderi <nderimichelle@gmail.com>
2023-03-11 22:46:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const body = document.getElementsByTagName("main")[0];
+const firstChild = document.querySelector('main#main');
+main.remove();
+
+var newHeader = document.createElement("h1",);
+newHeader.setAttribute("id","victory")
+document.body.append(newHeader);
+
+newHeader.innerHTML = "Michelle is the champion";
